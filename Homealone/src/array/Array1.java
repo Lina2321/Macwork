2178cb06f3540bc68774aab4ff7b5c846ea33ee5
lina <lina@218.38.137.28>
2021-10-23 02:00:15 +0900

    this is not array

---
+++ Homealone/src/array/Array1.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+				int b = 20;
+				int sum = a+b;
+				
+		System.out.println(sum);
+
+	}
+
+}
